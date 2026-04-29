4c7f302b2698038ed67758d0c515cd2bbb4a38be
dilmerprog <dilmernotebook9@gmail.com>
2026-04-29 01:28:47 -0300

    Criacao da calculadora

---
+++ ex01_calculator.py
@@ -0,0 +1,10 @@
+n1 = int(input('Numero 1:'))
+n2 = int(input('Numero 2:'))
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+print('O resultado de {} + {}= {}' .format(n1, n2, soma))
+print('O resultado de {} - {}= {}'. format(n1 , n2, subtracao))
+print('O Resultadod de {} * {}= {}'. format(n1, n2, multiplicacao))
+print('O resultado de {} / {}= {}'. format(n1, n2, divisao))
